f2bcaca262205ec3d1669cc7ab05174d5fc1b358
devkgn24@gmail.com <devkgn24@gmail.com>
2025-02-13 16:48:29 +0900

    [김가남] 회원가입 기능 구현
    	- 아이디, 비밀번호, 닉네임 필수 입력
    	- 비밀번호 암호화
    	- 모든 데이터 utf-8 인코딩

--- lec06_mvc/src/main/java/com/gn/member/controller/MemberCreateEndServlet.java
+++ lec06_mvc/src/main/java/com/gn/member/controller/MemberCreateEndServlet.java
@@ -1,11 +1,16 @@
 package com.gn.member.controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.gn.member.service.MemberService;
+import com.gn.member.vo.Member;
 
 @WebServlet(name="memberCreateEndServlet",urlPatterns = "/memberCreateEnd")
 public class MemberCreateEndServlet extends HttpServlet {
@@ -20,7 +25,22 @@ public class MemberCreateEndServlet extends HttpServlet {
 		String pw = request.getParameter("member_pw");
 		String name = request.getParameter("member_name");
 		
-		System.out.println(id+" : "+pw+ " : "+name);
+		//System.out.println(id+" : "+pw+ " : "+name);
+		Member m = new Member();
+		m.setMemberId(id);
+		m.setMemberPw(pw);
+		m.setMemberName(name);
+		
+		// Service에 데이터 전달
+		int result = new MemberService().createMember(m);
+		
+		RequestDispatcher view 
+			= request.getRequestDispatcher("/views/member/create_fail.jsp");
+		if(result > 0) {
+			view = request.getRequestDispatcher("/views/member/create_success.jsp");
+		}
+		view.forward(request, response);
+
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
