134b82ea42792e1d82b93f05395d9ad9a80d001c
devkgn24@gmail.com <devkgn24@gmail.com>
2025-02-20 15:02:14 +0900

    [김가남] 게시글 목록 조회

--- lec08_mybatis/src/main/java/com/gn/board/controller/BoardListServlet.java
+++ lec08_mybatis/src/main/java/com/gn/board/controller/BoardListServlet.java
@@ -9,6 +9,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.gn.board.service.BoardService;
+import com.gn.board.vo.Board;
+
 @WebServlet("/boardList")
 public class BoardListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -18,7 +21,8 @@ public class BoardListServlet extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//List<Board> = new BoardService().selectBoardList();
+		List<Board> resultList = new BoardService().selectBoardList();
+		System.out.println(resultList);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
