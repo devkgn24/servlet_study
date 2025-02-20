a6c432027df4731fc1b78aad57ded971efe32b86
devkgn24@gmail.com <devkgn24@gmail.com>
2025-02-20 15:52:52 +0900

    [김가남] mybatis를 사용하여 조회한 목록 출력하기

--- lec08_mybatis/src/main/java/com/gn/board/controller/BoardListServlet.java
+++ lec08_mybatis/src/main/java/com/gn/board/controller/BoardListServlet.java
@@ -3,6 +3,7 @@ package com.gn.board.controller;
 import java.io.IOException;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -22,7 +23,11 @@ public class BoardListServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		List<Board> resultList = new BoardService().selectBoardList();
-		System.out.println(resultList);
+		//System.out.println(resultList);
+		request.setAttribute("resultList", resultList);
+		RequestDispatcher view
+			= request.getRequestDispatcher("/views/board/list.jsp");
+		view.forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
